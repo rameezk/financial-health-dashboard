23f259c28a36a2b9dd9e125e41126be38a971874
Rameez Khan <rameezkhan.sa@gmail.com>
2020-06-02 07:36:45 +0200

    Add time helpers

--- src/financial_health_dashboard/example.cljs
+++ src/financial_health_dashboard/example.cljs
@@ -1,4 +1,11 @@
-(ns financial-health-dashboard.example)
+(ns financial-health-dashboard.example
+  (:require [cljs-time.core :as time]))
+
+(def now (time/now))
+
+(def this-year (time/year now))
+
+(def last-year (dec this-year))
 
 (def date-of-birth
   [["date-of-birth" 1985 11 6]])
