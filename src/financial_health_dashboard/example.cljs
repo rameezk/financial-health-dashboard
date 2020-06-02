b64f358f1c88de271883850d3a281cf0c005894e
Rameez Khan <rameezkhan.sa@gmail.com>
2020-06-02 12:33:35 +0200

    Add more data extractors

--- src/financial_health_dashboard/example.cljs
+++ src/financial_health_dashboard/example.cljs
@@ -1,5 +1,6 @@
 (ns financial-health-dashboard.example
-  (:require [cljs-time.core :as time]))
+  (:require [cljs-time.core :as time]
+            [clojure.string :as str]))
 
 (def now (time/now))
 
@@ -9,3 +10,19 @@
 
 (def date-of-birth
   [["date-of-birth" 1985 11 6]])
+
+(def year-goals
+  [["year-goal" this-year 15]
+   ["year-goal" this-year 13]
+   ["year-goal" this-year 11]])
+
+(def example-comment
+  [["comment" "some random comment"]])
+
+(def data-piped
+  (->> [date-of-birth
+        year-goals]
+       (reduce into)
+       (map #(->> % (map str) (str/join "|")))
+       (str/join "\n")))
+
