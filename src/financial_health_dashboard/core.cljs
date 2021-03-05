40512502cdc1d069b3581b921101719ab2274370
Rameez Khan <rameezkhan.sa@gmail.com>
2021-03-05 10:53:52 +0200

    Update shadow-cljs

---
+++ src/financial_health_dashboard/core.cljs
@@ -0,0 +1,18 @@
+(ns financial-health-dashboard.core
+  (:require [reagent.dom :as rd]))
+
+(defn ^:dev/after-load start []
+  (js/console.log "start"))
+
+(defn ^:export init []
+  (js/console.log "init")
+  (start))
+
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
+
+
+
+
+
+
