7478cfcb9ce5bb867d9e0f252a5cdb3f1a1c80ad
Rameez Khan <rameezkhan.sa@gmail.com>
2021-06-09 08:32:39 +0200

    Add more domain account functions

--- src/financial_health_dashboard/domain.cljs
+++ src/financial_health_dashboard/domain.cljs
@@ -1,24 +1,26 @@
 (ns financial-health-dashboard.domain)
 
-(def data {:user/name "Bob"
-           :user/age 28
-           :user/accounts [{:account/name "FNB Cheque Account"
-                            :account/type :bank
-                            :account/id 1}
-                           {:account/name "Easy Equities TFSA"
-                            :account/type :investment
-                            :account/id 2}]
-           :user/account-balances [{:account/id 1
-                                    :account-balance/year 2021
-                                    :accound-balance/month 6
+(def data {:user/name             "Bob"
+           :user/age              28
+           :user/accounts         [{:account/name   "FNB Cheque Account"
+                                    :account/type   :bank
+                                    :account/id     1
+                                    :account/active true}
+                                   {:account/name   "Easy Equities TFSA"
+                                    :account/type   :investment
+                                    :account/id     2
+                                    :account/active true}]
+           :user/account-balances [{:account/id             1
+                                    :account-balance/year   2021
+                                    :account-balance/month  6
                                     :account-balance/amount 100}
-                                   {:account/id 2
-                                    :account-balance/year 2021
-                                    :account-balance/month 6
+                                   {:account/id             2
+                                    :account-balance/year   2021
+                                    :account-balance/month  6
                                     :account-balance/amount 200}
-                                   {:account/id 2
-                                    :account-balance/year 2021
-                                    :account-balance/month 1
+                                   {:account/id             2
+                                    :account-balance/year   2021
+                                    :account-balance/month  1
                                     :account-balance/amount 400}]})
 
 
@@ -33,11 +35,13 @@
   age)
 
 (defn user-accounts
-  "Return a user's accounts, filtered if account-type is provided"
+  "Return a user's active accounts, filtered if account-type is provided"
   ([{:user/keys [accounts]}]
-   accounts)
+   (filter #(:account/active %) accounts))
   ([data account-type]
-   (filter #(= (:account/type %) account-type) (user-accounts data))))
+   (filter
+     #(= (:account/type %) account-type)
+     (user-accounts data))))
 
 (defn account
   "Return an account by provided account-id"
@@ -46,7 +50,7 @@
     (first (filter #(= (:account/id %) account-id) accounts))))
 
 (defn account-balances
-  "Returns all account balances, filtered if year and month are provided"
+  "Returns all account balances, filtered if year or month are provided"
   ([data]
    (:user/account-balances data))
 
@@ -56,6 +60,25 @@
 
   ([data year month]
    (filter #(and
-             (= (:account-balance/year %) year)
-             (= (:account-balance/month %) month))
+              (= (:account-balance/year %) year)
+              (= (:account-balance/month %) month))
            (account-balances data))))
+
+(defn balance-exists-for-account?
+  "Check if balance exists for account"
+  [account-id year month]
+  (let [balances             (account-balances data year month)
+        balances-for-account (filter #(= (:account/id %) account-id) balances)]
+    (pos? (count balances-for-account))))
+
+(defn net-worth
+  "Calculate net worth for year and month"
+  [data year month]
+  (reduce
+    (fn [acc
+         {:account-balance/keys [amount]}]
+      (+ acc amount))
+    0
+    (account-balances data year month)))
+
+
